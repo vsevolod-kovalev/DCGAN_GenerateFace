71602503fcaa1c0ad48538db5cdb043d4fdd4980
Vsevolod (V) Kovalev <87442802+vsevolod-kovalev@users.noreply.github.com>
2024-12-19 20:16:45 -0800

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -5,18 +5,16 @@ from Dense import Dense
 class Generator:
     def __init__(self, batch_size):
         self.layers = [
-            Dense(batch_size=batch_size, input_shape=(1, 1, 100), num_neurons=4 * 4 * 128, activation="relu"),
-            TConv2D(batch_size=batch_size, input_shape=(4, 4, 128), num_filters=64, kernel_size=4, stride=2, padding=1, activation="relu"),
-            TConv2D(batch_size=batch_size, input_shape=(8, 8, 64), num_filters=32, kernel_size=4, stride=2, padding=1, activation="relu"),
-            TConv2D(batch_size=batch_size, input_shape=(16, 16, 32), num_filters=16, kernel_size=4, stride=2, padding=1, activation="relu"),
-            TConv2D(batch_size=batch_size, input_shape=(32, 32, 16), num_filters=8, kernel_size=4, stride=2, padding=1, activation="relu"),
-            TConv2D(batch_size=batch_size, input_shape=(64, 64, 8), num_filters=3, kernel_size=3, stride=1, padding=1, activation="tanh")
+            Dense(batch_size=batch_size, input_shape=(1, 1, 100), num_neurons=8 * 8 * 256, activation="lrelu", batch_norm=True),
+            TConv2D(batch_size=batch_size, input_shape=(8, 8, 256), num_filters=128, kernel_size=4, stride=2, padding=1, activation="lrelu", batch_norm=True),
+            TConv2D(batch_size=batch_size, input_shape=(16, 16, 128), num_filters=64, kernel_size=4, stride=2, padding=1, activation="lrelu", batch_norm=True),
+            TConv2D(batch_size=batch_size, input_shape=(32, 32, 64), num_filters=3, kernel_size=4, stride=2, padding=1, activation="tanh")
         ]
-
+        
         self.W_deltas = [np.zeros_like(layer.W) if hasattr(layer, 'W') else None for layer in self.layers]
         self.B_deltas = [np.zeros_like(layer.B) if hasattr(layer, 'B') else None for layer in self.layers]
 
-    def applyDeltas(self, learning_rate=0.002):
+    def applyDeltas(self, learning_rate):
         for layer_index in range(len(self.layers)):
             if hasattr(self.layers[layer_index], 'W'):
                 self.layers[layer_index].W -= learning_rate * self.W_deltas[layer_index]
@@ -42,6 +40,11 @@ class Generator:
             if hasattr(self.layers[i], 'W'):
                 state[f"layer_{i}_W"] = layer.W
                 state[f"layer_{i}_B"] = layer.B
+            if hasattr(self.layers[i], 'scale'):
+                state[f"layer_{i}_scale"] = layer.scale
+                state[f"layer_{i}_shift"] = layer.shift
+                state[f"layer_{i}_running_mean"] = layer.running_mean
+                state[f"layer_{i}_running_var"] = layer.running_var
         return state
 
     def load_state_dict(self, state):
@@ -49,3 +52,8 @@ class Generator:
             if hasattr(self.layers[i], 'W'):
                 layer.W = state[f"layer_{i}_W"]
                 layer.B = state[f"layer_{i}_B"]
+            if hasattr(self.layers[i], 'scale'):
+                layer.scale = state[f"layer_{i}_scale"]
+                layer.shift = state[f"layer_{i}_shift"]
+                layer.running_mean = state[f"layer_{i}_running_mean"]
+                layer.running_var = state[f"layer_{i}_running_var"]
