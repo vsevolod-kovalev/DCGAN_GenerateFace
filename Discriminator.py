8940ed3d4047006c5bbc5320591961394585e749
Vsevolod (V) Kovalev <87442802+vsevolod-kovalev@users.noreply.github.com>
2024-12-19 20:17:13 -0800

    Update Discriminator.py

--- Discriminator.py
+++ Discriminator.py
@@ -2,24 +2,23 @@ import numpy as np
 from Conv2D import Conv2D
 from Dropout import Dropout
 from Dense import Dense
-from MinibatchDiscriminator import MinibatchDiscriminator
+
 
 class Discriminator:
     def __init__(self, batch_size):
         self.layers = [
-            Conv2D(batch_size=batch_size, input_shape=(64, 64, 3), num_filters=8, kernel_size=4, stride=2, padding=1, activation="lrelu"),
-            Conv2D(batch_size=batch_size, input_shape=(32, 32, 8), num_filters=16, kernel_size=4, stride=2, padding=1, activation="lrelu"),
-            Conv2D(batch_size=batch_size, input_shape=(16, 16, 16), num_filters=32, kernel_size=4, stride=2, padding=1, activation="lrelu"),
-            Conv2D(batch_size=batch_size, input_shape=(8, 8, 32), num_filters=64, kernel_size=4, stride=2, padding=1, activation="lrelu"),
-            Conv2D(batch_size=batch_size, input_shape=(4, 4, 64), num_filters=128, kernel_size=4, stride=2, padding=1, activation="lrelu"),
-            Dense(batch_size=batch_size, input_shape=(2, 2, 128), num_neurons=1, activation="sigmoid")
+            Conv2D(batch_size=batch_size, input_shape=(64, 64, 3), num_filters=64, kernel_size=4, stride=2, padding=1, activation="lrelu"),
+            Dropout(0.3),
+            Conv2D(batch_size=batch_size, input_shape=(32, 32, 64), num_filters=128, kernel_size=4, stride=2, padding=1, activation="lrelu"),
+            Dropout(0.3),
+            Dense(batch_size=batch_size, input_shape=(16, 16, 128), num_neurons=1, activation="sigmoid")
         ]
 
         self.W_deltas = [np.zeros_like(layer.W) if hasattr(layer, 'W') else None for layer in self.layers]
         self.B_deltas = [np.zeros_like(layer.B) if hasattr(layer, 'B') else None for layer in self.layers]
 
 
-    def applyDeltas(self, learning_rate=0.002):
+    def applyDeltas(self, learning_rate):
         for layer_index in range(len(self.layers)):
             if hasattr(self.layers[layer_index], 'W'):
                 self.layers[layer_index].W -= learning_rate * self.W_deltas[layer_index]
@@ -36,7 +35,10 @@ class Discriminator:
     def backward(self, gradient):
         current_gradient = gradient
         for layer_index in range(len(self.layers) -1, -1, -1):
-            current_gradient = self.layers[layer_index].backward(current_gradient, self.W_deltas[layer_index], self.B_deltas[layer_index])
+            if hasattr(self.layers[layer_index], 'W'):
+                current_gradient = self.layers[layer_index].backward(current_gradient, self.W_deltas[layer_index], self.B_deltas[layer_index])
+            else:
+                current_gradient = self.layers[layer_index].backward(current_gradient)
         return current_gradient
 
     def state_dict(self):
@@ -45,6 +47,11 @@ class Discriminator:
             if hasattr(self.layers[i], 'W'):
                 state[f"layer_{i}_W"] = layer.W
                 state[f"layer_{i}_B"] = layer.B
+            if hasattr(self.layers[i], 'scale'):
+                state[f"layer_{i}_scale"] = layer.scale
+                state[f"layer_{i}_shift"] = layer.shift
+                state[f"layer_{i}_running_mean"] = layer.running_mean
+                state[f"layer_{i}_running_var"] = layer.running_var
         return state
 
     def load_state_dict(self, state):
@@ -52,3 +59,8 @@ class Discriminator:
             if hasattr(self.layers[i], 'W'):
                 layer.W = state[f"layer_{i}_W"]
                 layer.B = state[f"layer_{i}_B"]
+            if hasattr(self.layers[i], 'scale'):
+                layer.scale  =state[f"layer_{i}_scale"]
+                layer.shift = state[f"layer_{i}_shift"]
+                layer.running_mean = state[f"layer_{i}_running_mean"]
+                layer.running_var = state[f"layer_{i}_running_var"]
