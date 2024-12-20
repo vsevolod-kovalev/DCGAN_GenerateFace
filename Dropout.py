6c42b0f57ac25629592380c59726f3c9a61f54c8
Vsevolod (V) Kovalev <87442802+vsevolod-kovalev@users.noreply.github.com>
2024-12-19 20:17:00 -0800

    Update Dropout.py

--- Dropout.py
+++ Dropout.py
@@ -6,6 +6,6 @@ class Dropout:
     def forward(self, input):
         self.mask = np.random.binomial(1, 1 - self.dropout, np.shape(input))
         return input * self.mask / (1 - self.dropout)
-    def backward(self, gradient, _, __):
+    def backward(self, gradient):
         return self.mask * gradient
 
